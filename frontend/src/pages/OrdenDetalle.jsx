b5504378ade6c61d5de5ee78a58373b809bff207
Randy Simanca <randysimancamercado@gmail.com>
2026-05-07 17:09:34 -0500

    correccion de errores

--- frontend/src/pages/OrdenDetalle.jsx
+++ frontend/src/pages/OrdenDetalle.jsx
@@ -19,7 +19,7 @@ import { confirmDelete, successAlert } from '../lib/alerts'
 const generateUUID = () => {
   try {
     return crypto.randomUUID()
-  } catch (e) {
+  } catch {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
       const r = Math.random() * 16 | 0
       const v = c === 'x' ? r : (r & 0x3 | 0x8)
